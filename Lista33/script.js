33a5ebc3832306d49c08e5386201db9e2fb29ad6
Alexandre Lipari <santuslipari@gmail.com>
2022-10-11 12:45:30 -0300

    Exercício 1 exibir números de vogais,2 exibir letras do meio de uma palavra sendo par ou impar, 3 ordenar um numero inteiro decrescente.

---
+++ Lista33/script.js
@@ -0,0 +1,28 @@
+// let letra:string
+// letra = "Alexandre"
+// console.log(ProcurarVogais(letra))
+// function ProcurarVogais(letra:string):number{
+//    let resultado = letra.match(/[aeiou]/ig)
+//     return resultado.length
+// }
+// let numero:string = ""
+// numero = "45694329"
+// console.log(OrganizarDigitos(numero))
+// function OrganizarDigitos(numero:string){
+//     let resultado = numero.split("")
+//     resultado.sort((a,b) => parseInt (b)-parseInt(a))
+//     return parseInt( resultado.join(""))
+// }
+var palavra = "";
+palavra = "parametros";
+console.log(PosicaoDoMeio(palavra));
+function PosicaoDoMeio(palavra) {
+    var tamanho = palavra.length;
+    console.log(tamanho);
+    if (tamanho % 2 == 0) {
+        console.log(palavra[(tamanho / 2) - 1], palavra[(tamanho / 2)]);
+    }
+    else {
+        console.log(palavra[Math.floor(tamanho / 2)]);
+    }
+}
